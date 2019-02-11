d65697c3ceb3be3a7fd61b8a6c55b48309c6d3c9
Jerico Pingul <jerico.pingul@gmail.com>
2019-02-11 21:47:36 +0000

    Cookies

--- app.js
+++ app.js
@@ -32,35 +32,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
 
-  let authHeader = req.headers.authorization;
+  console.log(req.signedCookies);
 
-  if(!authHeader) {
-    const err = new Error('You are not authenticated!');
+  if(!req.signedCookies.user) {
+    console.log(req.headers);
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-
-  // auth should be array of 2 items - username and password
-  const auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-  const username = auth[0];
-  const password = auth[1];
-
-  if(username === 'admin' && password === 'password') {
-    next(); // pass request to next middleware
+    let authHeader = req.headers.authorization;
+  
+    if(!authHeader) {
+      const err = new Error('You are not authenticated!');
+  
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  
+    // auth should be array of 2 items - username and password
+    const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  
+    const username = auth[0];
+    const password = auth[1];
+  
+    if(username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', { signed: true });
+      next(); // pass request to next middleware
+    } else {
+      const err = new Error('You are not authenticated!');
+  
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    const err = new Error('You are not authenticated!');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+    if(req.signedCookies.user === 'admin') {
+      next();
+    } else {
+      const err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
