2908dc6c6c27c0dc52e85880f6bba847008ba419
Jerico Pingul <jerico.pingul@gmail.com>
2019-02-19 21:10:26 +0000

    Mongoose Population

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter.use(bodyParser.json())
 dishRouter.route('/')
   .get((req, res, next) => {
     Dishes.find({})
+      .populate('comments.author')
       .then(dishes => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
@@ -46,6 +47,7 @@ dishRouter.route('/')
 dishRouter.route('/:dishId')
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate('comments.author')
       .then(dish => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -81,6 +83,7 @@ dishRouter.route('/:dishId')
   dishRouter.route('/:dishId/comments')
   .get((req, res, next) => {
     Dishes.findById(req.params.dishId)
+      .populate('comments.author')
       .then(dish => {
           if(dish != null) {
             res.statusCode = 200;
@@ -98,12 +101,17 @@ dishRouter.route('/:dishId')
     Dishes.findById(req.params.dishId)
       .then(dish => {
         if(dish != null) {
+          req.body.author = req.user._id;
           dish.comments.push(req.body);
           dish.save()
             .then(dish => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish);
+              Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then(dish => {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(dish);
+                });
             });
         } else {
           err = new Error(`Dish ${req.params.dishId} not found`);
@@ -142,7 +150,8 @@ dishRouter.route('/:dishId')
 
 dishRouter.route('/:dishId/comments/:commentId')
   .get((req, res, next) => {
-   Dishes.findById(req.params.dishId)
+    Dishes.findById(req.params.dishId)
+      .populate('comments.author')
       .then(dish => {
         if(dish != null && dish.comments.id(req.params.commentId) != null) {
           res.statusCode = 200;
@@ -177,11 +186,15 @@ dishRouter.route('/:dishId/comments/:commentId')
           }
           dish.save()
             .then(dish => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish);
-            });
-        } else if(dish == null){
+              Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then(dish => {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(dish);
+                });
+            }, err => next(err));
+        } else if(dish == null) {
           err = new Error(`Dish ${req.params.dishId} not found`);
           err.status = 404;
           return next(err);
@@ -200,9 +213,13 @@ dishRouter.route('/:dishId/comments/:commentId')
           dish.comments.id(req.params.commentId).remove();
           dish.save()
             .then(dish => {
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.json(dish);
+              Dishes.findById(dish._id)
+                .populate('comments.author')
+                .then(dish => {
+                  res.statusCode = 200;
+                  res.setHeader('Content-Type', 'application/json');
+                  res.json(dish);
+                });
             }, err => next(err))
         } else if(dish == null){
           err = new Error(`Dish ${req.params.dishId} not found`);
--- routes/users.js
+++ routes/users.js
@@ -19,13 +19,27 @@ router.post('/signup', (req, res, next) => {
       res.setHeader('Content-Type', 'application/json');
       res.json({ err: err })
     } else {
-      passport.authenticate('local')(req, res, () => { // authenticate same user
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ success: true, status: 'Registration Successful!' });
-      }); 
+      if(req.body.firstname) {
+        user.firstname = req.body.firstname;
+      }
+      if(req.body.lastname) {
+        user.lastname = req.body.lastname;
+      }
+      user.save((err, user) => {
+        if(err) {
+          rres.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ err: err });
+          return;
+        }
+        passport.authenticate('local')(req, res, () => { // authenticate same user
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        }); 
+      });
     }
-  })
+  });
 });
 
 // authenticate local automatically sends error to client if function is in eror
