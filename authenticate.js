8343683b8ac25cc70de102b7f60190261d706ffc
Jerico Pingul <jerico.pingul@gmail.com>
2019-03-13 21:49:20 +0000

    Assignment 3

--- authenticate.js
+++ authenticate.js
@@ -12,9 +12,10 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// creates the json web token
 exports.getToken = function(user) {
   return jwt.sign(user, config.secretKey, {
-    expiresIn: 3600 // an hour later you have to renew the jwt
+    expiresIn: 30*24*3600 // an hour later you have to renew the jwt
   })
 };
 
@@ -22,6 +23,8 @@ const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
+// done callback provided by passport - passing back info to passport to load to request message 
+// done takes 3 parameters, 
 exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
   console.log('JWT payload', jwt_payload);
   User.findOne({ _id: jwt_payload._id }, (err, user) => {
@@ -35,5 +38,23 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => 
   })
 }));
 
+
+// used to verify an incoming user
+// passport.authenticate - jwt strategy configured above
+// session is false because we are using token-based authentication
 exports.verifyUser = passport.authenticate('jwt', { session: false });
 
+
+// verifyUser loads new property named user to req with user details
+exports.verifyAdmin = (req, res, next) => {
+  // console.log('***req.user***', req.user)
+  User.findOne({ username: req.user.username }, function (err, user) {
+    if (err || !req.user.admin) {
+      err = new Error(`You are not authorized to perform this operation!`);
+      err.status = 403;
+      return next(err);
+    }
+    // Pass to next
+    next();
+  });
+}
